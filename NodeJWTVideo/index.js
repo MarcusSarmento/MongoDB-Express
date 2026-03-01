9feba0e2af5eba475c533a21f92bb5d85ce3aa67
Marcus <mvddssarmento@hotmail.com>
2026-02-28 22:07:09 -0300

    Aula 2 : Fazendo os primeiros pedidos

--- NodeJWTVideo/index.js
+++ NodeJWTVideo/index.js
@@ -1,4 +1,14 @@
 import nodemon from 'nodemon'
+import express from 'express'
 
-console.log('Hello World')
+const app = express();
 
+app.listen(process.env.PORT || 3000);
+
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+});
